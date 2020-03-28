9462161ad7c28535a2a9c5a0747f3e0e7557e238
toni ivanov <toniivanov03@gmail.com>
2020-03-28 18:52:11 +0200

    refactoring

--- Templating/01. List Towns/app.js
+++ Templating/01. List Towns/app.js
@@ -3,8 +3,10 @@ const towns = document.getElementById('towns');
 
 document.getElementById('btnLoadTowns').addEventListener('click', onLoadClick);
 
-function onLoadClick() {
-    const template = Handlebars.compile(document.getElementById('towns-template').textContent);
+async function onLoadClick() {
+    const t = await fetch('./template.hbs').then(r => r.text());
+
+    const template = Handlebars.compile(t);
 
     root.innerHTML = template({towns: towns.value.split(', ')})
 }
